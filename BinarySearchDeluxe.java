2e9c09adc1dfff01294fc794e9113c9f4abbd3af
Charles Werk <charles.werk@umconnect.umt.edu>
2023-03-02 23:01:07 -0700

    Finished Term.java

--- BinarySearchDeluxe.java
+++ BinarySearchDeluxe.java
@@ -3,23 +3,56 @@ import java.util.Comparator;
 
 public class BinarySearchDeluxe {
 
-    
+
     // Returns the index of the first key in a[] that equals the search key, or -1 if no such key.
     public static <Key> int firstIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
+        if (isValid(a, key, comparator)) ; // check for valid entry
+        // Binary search implementation
+        int index = 0;
+        int lo = 0;
+        int hi = a.length - 1;
+        // check if the first key is equal to the given key
+        if (comparator.compare(a[0], key) == 0)
+            return 0;
+        while (lo <= hi) {
+            int mid = lo + (hi - lo) / 2;
+            // key is in left half
+            if (comparator.compare(key, a[mid]) < 0) {
+                hi = mid - 1;
+            }// key is in right half
+            else if (comparator.compare(key, a[mid]) > 0) {
+                lo = mid + 1;
+            } // if 2 keys are the same
+            else if (comparator.compare(a[mid - 1], a[mid]) > 0) {
+                hi = mid - 1;
+            }
+            else return mid;
+
+        }
 
         return -999; //FIXME
-           
+
     }
 
-    
+
     // Returns the index of the last key in a[] that equals the search key, or -1 if no such key.
     public static <Key> int lastIndexOf(Key[] a, Key key, Comparator<Key> comparator) {
         return -999; //FIXME
     }
 
+    private static <Key> boolean isValid(Key[] a, Key key, Comparator<Key> comparator) {
+        if (a == null || key == null || comparator == null) {
+            throw new IllegalArgumentException("Can't have null values.");
+        }
+        else {
+            return true;
+        }
+    }
+
+
     // unit testing (you should have some Unit Testing here to confirm that your methods work); for example...
     public static void main(String[] args) {
-    
+
         Term[] terms = new Term[5];
         terms[0] = new Term("Trevor", 45);
         terms[1] = new Term("Kathy", 43);
@@ -27,25 +60,25 @@ public class BinarySearchDeluxe {
         terms[3] = new Term("Allen", 9);
         terms[4] = new Term("Eva", 1);
         Arrays.sort(terms);
-        
-        Term searchme = new Term("J",0);
+
+        Term searchme = new Term("J", 0);
         int first = BinarySearchDeluxe.firstIndexOf(terms, searchme, Term.byPrefixOrder(1));
         int last = BinarySearchDeluxe.lastIndexOf(terms, searchme, Term.byPrefixOrder(1));
         StdOut.println("J: " + first + " to " + last);
 
-        searchme = new Term("A",0);
+        searchme = new Term("A", 0);
         first = BinarySearchDeluxe.firstIndexOf(terms, searchme, Term.byPrefixOrder(1));
         last = BinarySearchDeluxe.lastIndexOf(terms, searchme, Term.byPrefixOrder(1));
         StdOut.println("A: " + first + " to " + last);
-       
-        searchme = new Term("E",0);
+
+        searchme = new Term("E", 0);
         first = BinarySearchDeluxe.firstIndexOf(terms, searchme, Term.byPrefixOrder(1));
         last = BinarySearchDeluxe.lastIndexOf(terms, searchme, Term.byPrefixOrder(1));
         StdOut.println("E: " + first + " to " + last);
-        
-        searchme = new Term("T",0);
+
+        searchme = new Term("T", 0);
         first = BinarySearchDeluxe.firstIndexOf(terms, searchme, Term.byPrefixOrder(1));
         last = BinarySearchDeluxe.lastIndexOf(terms, searchme, Term.byPrefixOrder(1));
-        StdOut.println("T: " + first + " to " + last);        
+        StdOut.println("T: " + first + " to " + last);
     }
 }
--- Term.java
+++ Term.java
@@ -2,22 +2,27 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public class Term implements Comparable<Term> {
-    
+    String QUERY;
+    long WEIGHT;
+
     // Initializes a term with the given query string and weight.
     public Term(String query, long weight) {
-
+        QUERY = query;
+        WEIGHT = weight;
+        // int QLen = QUERY.length();
     }
 
     public static Comparator<Term> byReverseWeightOrder() {
         return new Comparator<Term>() {
             public int compare(Term v, Term w) {
-                //some condition
-                    return -1;
-                //some condition
-                    return 1;
-                //some condition
-                    return 0;
+                //if v > w
+                if (v.WEIGHT > w.WEIGHT) return -1;
+                //if w > v
+                if (v.WEIGHT < w.WEIGHT) return 1;
+                //if v == w
+                if (v.WEIGHT == w.WEIGHT) return 0;
 
+                return 0;
             }
 
         };
@@ -26,21 +31,33 @@ public class Term implements Comparable<Term> {
     // Compares the two terms in lexicographic order but using only the first r characters of each query.
     public static Comparator<Term> byPrefixOrder(int r) {
         return new Comparator<Term>() {
+            String sub1;
+            String sub2;
+
             public int compare(Term v, Term w) {
-                //FIXME
+                if (r > v.QUERY.length()) {
+                    sub1 = v.QUERY;
+                    sub2 = w.QUERY;
+                }
+                else {
+                    sub1 = v.QUERY.substring(0, r);
+                    sub2 = w.QUERY.substring(0, r);
+                }
+                return sub1.compareTo(sub2);
+
             }
         };
     }
 
     // Compares the two terms in lexicographic order by query.
     public int compareTo(Term that) {
-        return 0; //FIXME
+        return QUERY.compareTo(that.QUERY);
     }
 
     // Returns a string representation of this term in the following format:
     // the weight, followed by a tab, followed by the query.
     public String toString() {
-        return null; //FIXME
+        return (WEIGHT + "\t" + QUERY);
     }
 
     // unit testing (you should have some Unit Testing here to confirm that your methods work); for example...
@@ -51,22 +68,22 @@ public class Term implements Comparable<Term> {
         terms[2] = new Term("Ellie", 11);
         terms[3] = new Term("Allen", 9);
         terms[4] = new Term("Eva", 1);
-        
+
         Arrays.sort(terms);
         for (Term t : terms) {
             StdOut.println(t);
         }
-        
+
         StdOut.println("");
         Arrays.sort(terms, Term.byReverseWeightOrder());
         for (Term t : terms) {
             StdOut.println(t);
         }
-        
+
         StdOut.println("");
         Arrays.sort(terms, Term.byPrefixOrder(1));
         for (Term t : terms) {
             StdOut.println(t);
-        }        
+        }
     }
 }
